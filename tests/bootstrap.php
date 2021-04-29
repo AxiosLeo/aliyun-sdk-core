8c8b315ededeef636a3a7d59ae7b31b80e25e0b7
axios <axiosleo@foxmail.com>
2021-04-30 00:24:46 +0800

    fix: ci

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,9 +3,3 @@
 declare(strict_types=1);
 
 require dirname(__DIR__) . \DIRECTORY_SEPARATOR . 'vendor' . \DIRECTORY_SEPARATOR . 'autoload.php';
-
-$rootPath = dirname(__DIR__) . \DIRECTORY_SEPARATOR;
-if (file_exists($rootPath . '.env')) {
-    $dotenv = Dotenv\Dotenv::createImmutable($rootPath);
-    $dotenv->load();
-}
